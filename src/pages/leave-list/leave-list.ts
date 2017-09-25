566cb978c59575bc79e1c64398e4b2dd317c2589
Zema-Zima <blackflower225@gmail.com>
2017-09-25 16:55:01 +0200

    add leaves operations

--- src/pages/leave-list/leave-list.ts
+++ src/pages/leave-list/leave-list.ts
@@ -1,22 +1,47 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { RequestLeavePage} from '../request-leave/request-leave';
+import { RequestLeavePage } from '../request-leave/request-leave';
+import { LeaveServicesApi, IRequestType } from '../../shared/LeavesService';
+
+enum approvalStatusEnum {
+  New = 1, Submit, AprovalEmployeeReview, ManagerReview, Accepted, Approved, CancelBeforeAccepted, CancelAfterAccepted, Rejected
+}
 
 @IonicPage()
 @Component({
   selector: 'page-leave-list',
   templateUrl: 'leave-list.html',
 })
+
 export class LeaveListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  RequestTypeObj: IRequestType = {
+    CompId: 0,
+    Culture: "ar-EG",
+    EmpId: 1072
+    //17 
+  }
+  public LeavesData: Array<any> = [];
+  public Status: string = "";
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public LeaveServices: LeaveServicesApi) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LeaveListPage');
   }
+  ionViewWillEnter() {
+    this.LeaveServices.getLeaves(this.RequestTypeObj).subscribe((data) => {
+      console.log("getLeavesdata ",data);
+      this.LeavesData = data;
+    }, (e) => {
+      console.log("error ", e);
+    })
+  }
 
-  addLeave(){
+  addLeave() {
     this.navCtrl.push(RequestLeavePage);
   }
 
--- src/pages/request-leave/request-leave.ts
+++ src/pages/request-leave/request-leave.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the RequestLeavePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { LeaveServicesApi, IRequestType, IRequestData } from '../../shared/LeavesService';
+import { LeaveListPage } from '../leave-list/leave-list';
 @IonicPage()
 @Component({
   selector: 'page-request-leave',
@@ -15,11 +10,61 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RequestLeavePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  RequestTypeObj: IRequestType = {
+    CompId: 0,
+    Culture: "ar-EG",
+    EmpId: 1072
   }
+  RequestDataObj: IRequestData = {
+    CompanyId: 0,
+    TypeId: 1067,
+    Culture: "ar-EG",
+    EmpId: 1072,
+    RequestId: 0,
+    StartDate: "2017-09-24 00:00:00.000"
+  }
+  public RequestLeaveForm: FormGroup;
+  public LeavesData: Array<any> = [];
+  public leaving: any;
+  public AllowedDays: number = 0;
+  public Replace:Array<any>=[];
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public LeaveServices: LeaveServicesApi,
+    private formBuilder: FormBuilder) {
+   // this.leaving = 1067;  //annual leave 
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RequestLeavePage');
   }
+
+  ionViewWillEnter() {
+    console.log("leaving ", this.leaving);
+    this.LeaveServices.GetLeaveTypes(this.RequestTypeObj).subscribe((data) => {
+      this.LeavesData = data;
+    }, (e) => {
+      console.log("error ", e);
+    })
+  }
+  /////////////////////
+  leaveChange(item: any) {
+    console.log("itemSelected ", item);
+    this.RequestDataObj.TypeId = item;
+    this.RequestDataObj.StartDate = "2017-09-24 00:00:00.000";
+    this.LeaveServices.GetRequestLeaveData(this.RequestDataObj).subscribe((data) => {
+      console.log("data GetRequestLeaveData ", data);
+      this.AllowedDays = data.requestVal.AllowedDays;
+      this.Replace=data.Replacements;
+      console.log("rrrrrrr ",this.Replace)
+    }, (err) => {
+      console.log("error ", err)
+    })
+  }
+  saveLeaves() {
+    this.navCtrl.push(LeaveListPage);
+  }
+
 
 }
